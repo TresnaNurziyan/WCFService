b923e920bd3530c78238b020b8c2546cff24ebb0
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2017-08-01 10:17:04 +0700

    WCF Services application

---
+++ WcfService/IService1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+        // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+        [OperationContract]
+        [WebGet(UriTemplate = "users", ResponseFormat = WebMessageFormat.Json)]
+        List<User> GetUsersList();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "user/{id}", ResponseFormat = WebMessageFormat.Json)]
+        User GetUserById(string id);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "user/{email}/{pass}", ResponseFormat = WebMessageFormat.Json)]
+        bool IsValid(string email, string pass);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "user/add/{name}/{email}/{pass}", ResponseFormat = WebMessageFormat.Json)]
+        void AddUser(string name, string email, string pass);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "user/update", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        void UpdateUser(UserComposite user);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "user/delete/{id}", ResponseFormat = WebMessageFormat.Json)]
+        void DeleteUser(string id);
+
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class UserComposite
+    {
+        private int userId;
+        private string userName;
+        private string userEmail;
+        private string userPass;
+
+        [DataMember(Name = "id")]
+        public int Id
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        [DataMember(Name = "email")]
+        public string Email
+        {
+            get { return userEmail; }
+            set { userEmail = value; }
+        }
+        [DataMember(Name = "password")]
+        public string Password
+        {
+            get { return userPass; }
+            set { userPass = value; }
+        }
+
+        //bool boolValue = true;
+        //string stringValue = "Hello ";
+
+        //[DataMember]
+        //public bool BoolValue
+        //{
+        //    get { return boolValue; }
+        //    set { boolValue = value; }
+        //}
+
+        //[DataMember]
+        //public string StringValue
+        //{
+        //    get { return stringValue; }
+        //    set { stringValue = value; }
+        //}
+    }
+}
---
+++ WcfService/Service1.svc.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<User> GetUsersList()
+        {
+            using (WCFServiceApplicationEntities entities = new WCFServiceApplicationEntities())
+            {
+                return entities.User.ToList();
+            }
+        }
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        public User GetUserById(string id)
+        {
+            try
+            {
+                int UserId = Convert.ToInt32(id);
+
+                using (WCFServiceApplicationEntities entities = new WCFServiceApplicationEntities())
+                {
+                    return entities.User.SingleOrDefault(User => User.id == UserId);
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+
+        public bool IsValid(string em, string ps)
+        {
+            try
+            {
+                string UserName = em;
+                string UserPass = ps;
+
+                using (WCFServiceApplicationEntities entities = new WCFServiceApplicationEntities())
+                {
+                    var query = (from c in entities.User
+                                 where c.email == em && c.password == ps
+                                 select c).Any();
+                    return query;
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+
+        public void AddUser(string name, string email, string password)
+        {
+            using (WCFServiceApplicationEntities entities = new WCFServiceApplicationEntities())
+            {
+                User book = new User { name = name, email = email, password = password };
+                entities.User.Add(book);
+                entities.SaveChanges();
+            }
+        }
+
+        public void UpdateUser(UserComposite usr)
+        {
+            int UserId = Convert.ToInt32(usr.Id);
+
+            using (WCFServiceApplicationEntities entities = new WCFServiceApplicationEntities())
+            {
+                User wow = entities.User.SingleOrDefault(b => b.id == UserId);
+                wow.name = usr.Name;
+                wow.email = usr.Email;
+                wow.password = usr.Password;
+                entities.SaveChanges();
+            }
+        }
+
+        public void DeleteUser(string id)
+        {
+            try
+            {
+                int UserId = Convert.ToInt32(id);
+
+                using (WCFServiceApplicationEntities entities = new WCFServiceApplicationEntities())
+                {
+                    User User = entities.User.SingleOrDefault(b => b.id == UserId);
+                    entities.User.Remove(User);
+                    entities.SaveChanges();
+                }
+            }
+            catch
+            {
+                throw new FaultException("Something went wrong");
+            }
+        }
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+    }
+}
